dfc0b3a98e0c5d448bf5f8795d07797bafdfb89c
wangbo <wangbo@imusics.net>
2016-12-31 02:20:08 +0800

    modify setting

--- oj/custom_settings.py
+++ oj/custom_settings.py
@@ -2,9 +2,9 @@
 import os
 
 
-WEBSITE_INFO = {"website_name": u"example大学 OnlineJudge",
-                "website_name_shortcut": u"example oj",
-                "website_footer": u"example大学foobar学院 <a href=\"http://www.miibeian.gov.cn/\">京ICP备233333号-1</a>",
+WEBSITE_INFO = {"website_name": u"北京林业大学在线评测系统",
+                "website_name_shortcut": u"BjfuOnlineJudge",
+                "website_footer": u"北京林业大学大学信息学院 <a href=\"http://www.miibeian.gov.cn/\">京ICP备233333号-1</a>",
                 # url结尾没有/
                 "url": u"http://your-domain-or-ip.com"}
 
@@ -18,7 +18,7 @@ SMTP_CONFIG = {"smtp_server": "smtp.domain.com",
 
 
 # 是否显示所有人的提交, False就只显示自己的
-SHOW_ALL_SUBMISSIONS_LIST = False
+SHOW_ALL_SUBMISSIONS_LIST = True
 
 
 # please set your own SECRET_KEY to a long random string
