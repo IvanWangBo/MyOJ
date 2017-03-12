8c0343aebb2cc8c575a2a037781b2d6f79587cf5
zhangjianglan <zhangjianglan@imusics.net>
2017-03-13 02:15:41 +0800

    add batch request join group function fix a data get for up function

--- group/serializers.py
+++ group/serializers.py
@@ -41,7 +41,7 @@ class JoinGroupRequestSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = JoinGroupRequest
-        fields = ['group', 'user']
+        fields = ['id', 'group', 'user']
 
 
 class GroupSerializer(serializers.ModelSerializer):
