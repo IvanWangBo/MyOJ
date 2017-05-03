b39a786adb81db323191a6a6f07e98554a651c15
jokerq <17710281664@163.com>
2017-05-03 23:30:29 +0800

    final

--- static/src/js/app/oj/contest/push_announce.js
+++ static/src/js/app/oj/contest/push_announce.js
@@ -3,11 +3,7 @@ require(["jquery", "bsAlert", "csrfToken", "validator"], function ($, bsAlert, c
 	$("#announcement_form").width( $(window).width() * 0.75 );
 	$("#announce_content").width( $("#announcement_form").width() );
 	$("#announce_content").height( $("#announce_content").width() * 0.75 );
-	$("#btn_push_announce").css({
-		"margin":"auto",
-		"margin-bottom":"5px"
-	});
-	$("#announcement_form").css("text-align","center");
+	$("#btn_push_announce").css("margin-bottom","5px");
 
 	var btn_push = $("#btn_push_announce");
 
