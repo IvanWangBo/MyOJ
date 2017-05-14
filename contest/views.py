1529539ecf0fab46a19aa8aaa80c5bf30dce82aa
jokerq <17710281664@163.com>
2017-05-14 21:27:32 +0800

    implements: contest AC code download

--- contest/views.py
+++ contest/views.py
@@ -706,3 +706,26 @@ def push_announcement_page(request, contest_id):
     show push_announcement_page 
     """
     return render(request, "oj/contest/push_announcement.html", {"contest_id": contest_id})
+
+def DownloadContestCode(request, contest_id):
+local_url = r'/home/OnlineJudge/contest_code/' + str(contest_id) + '.txt'
+    if not os.path.exists(local_url):
+        file = open(local_url, 'a')
+        submissions = Submission.objects.filter(contest_id=contest_id, result=0). \
+        order_by("-create_time").values("id", "user_id", "result", "create_time", "code", "language")
+        for sub in submissions:
+            try:
+                user = User.objects.get(id=sub.user_id)
+                this_username = user.username
+            except User.DoesNotExist:
+                this_username = "none"
+            file.write("用户名： " + this_username + '\n')
+            file.write("提交id： " + sub.id + '\n')
+            file.write("提交状态：(0 为通过) " + sub.result + '\n')
+            file.write("通过时间： " + sub.create_time + '\n')
+            file.write("编程语言： " + sub.language + '\n')
+            file.write("code: " + '\n' + sub.code + '\n' + '\n')
+        return HttpResponse(file.read())
+    else:
+        file = open(local_url)
+        return HttpResponse(file.read())
