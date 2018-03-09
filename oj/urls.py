a9f97fbaf1201d74086d125d5f662d8bb0588b87
wangbo <wangbo@imusics.net>
2018-03-09 22:54:05 +0800

    fireworks

--- oj/urls.py
+++ oj/urls.py
@@ -149,4 +149,5 @@ urlpatterns = [
     url(r'^rank/$', "account.views.user_rank_page", name="user_rank_page"),
     url(r'^api/avatar/upload/', AvatarUploadAPIView.as_view(), name="avatar_upload_api"),
     url(r'^woaixiaoyueyue/', TemplateView.as_view(template_name="other/chouqian.html"), name="chouqian"),
+    url(r'^woaixiaoyueyue/', TemplateView.as_view(template_name="other/fireworks.html"), name="fireworks"),
 ]
