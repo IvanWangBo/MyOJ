3cfc48548d74c5f18ce1e988e92ef2a270bdb730
wangbo <wangbo@imusics.net>
2018-03-09 23:12:34 +0800

    fix

--- oj/urls.py
+++ oj/urls.py
@@ -149,6 +149,6 @@ urlpatterns = [
     url(r'^rank/$', "account.views.user_rank_page", name="user_rank_page"),
     url(r'^api/avatar/upload/', AvatarUploadAPIView.as_view(), name="avatar_upload_api"),
     url(r'^woaixiaoyueyue/chouqian/$', TemplateView.as_view(template_name="other/chouqian.html"), name="chouqian"),
-    url(r'^woaixiaoyueyue/fireworks/$t', TemplateView.as_view(template_name="other/fireworks.html"), name="fireworks"),
-    url(r'^woaixiaoyueyue/fireworks2/$t', TemplateView.as_view(template_name="other/fireworks2.html"), name="fireworks2"),
+    url(r'^woaixiaoyueyue/fireworks/$', TemplateView.as_view(template_name="other/fireworks.html"), name="fireworks"),
+    url(r'^woaixiaoyueyue/fireworks2/$', TemplateView.as_view(template_name="other/fireworks2.html"), name="fireworks2"),
 ]
